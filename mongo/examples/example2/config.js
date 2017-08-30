06c40a645a3468a4aa34b9d0b6d3546c793d3e86
Sharif <sharif.malik@neptune-int.com>
2017-08-30 23:28:35 +0530

    added second example of mongoose

---
+++ mongo/examples/example2/config.js
@@ -0,0 +1,7 @@
+var mongo = {
+	'url' : 'mongodb://localhost:27017/mongoose-test',
+	'host' : '127.0.0.1',
+	'database' : 'mongoose-test'
+};
+
+module.exports.mongo = mongo;
---
+++ mongo/examples/example2/userModel.js
@@ -0,0 +1,22 @@
+// Mongoose import
+var mongoose = require('mongoose');
+var config = require('./config.js');
+
+// Mongoose connection to MongoDB
+mongoose.connect(config.mongo.url,{useMongoClient: true},  function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
+// Mongoose Schema definition
+var UserSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    emailId: String
+});
+
+// Mongoose Model definition
+var userModel = mongoose.model('user', UserSchema);
+
+module.exports.userModel = userModel;
